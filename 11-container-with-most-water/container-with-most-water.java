e1121aa77096717374b7c9a36ca47d26a2db3700
Amanrawal1305 <141016643+Amanrawal1305@users.noreply.github.com>
2025-06-28 14:07:26 +0530

    Time: 2 ms (98.92%) | Memory: 58.7 MB (8.20%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -1,27 +1,49 @@
+// class Solution {
+//     public int maxArea(int[] height) {
+//        // ArrayList<Integer> height=new ArrayList<>();
+//         int n = height.length;
+//         int str =0;
+//         int end= n -1;
+//         int maxarr=0;
+//         while(str<end){
+//             int ht = Math.min(height[str],height[end]);
+//             int w = end - str;
+//             int area = ht * w;
+//             maxarr= Math.max(maxarr,area);
+        
+//             if(height[str]>height[end]){
+//                 end--;
+//             }
+//             // else if(height[str]<height[end]){
+//             //     str++;
+//             // }
+//             else{
+//                 str++;
+//                // end--;
+//             } 
+//         } 
+//         return maxarr;  
+//     }
+// }
+
 class Solution {
     public int maxArea(int[] height) {
-       // ArrayList<Integer> height=new ArrayList<>();
-        int n = height.length;
-        int str =0;
-        int end= n -1;
-        int maxarr=0;
-        while(str<end){
-            int ht = Math.min(height[str],height[end]);
-            int w = end - str;
-            int area = ht * w;
-            maxarr= Math.max(maxarr,area);
-        
-            if(height[str]>height[end]){
-                end--;
+        int max = 0;
+        int left = 0;
+        int right = height.length - 1;
+
+        while (left < right) {
+            int h = Math.min(height[left], height[right]);
+            max = Math.max(max, h * (right-left));
+
+             while(left < right && height[left] <= h){
+                left++;
             }
-            // else if(height[str]<height[end]){
-            //     str++;
-            // }
-            else{
-                str++;
-               // end--;
-            } 
-        } 
-        return maxarr;  
+            while(left < right && height[right] <= h){
+                right--;
+            }
+        }
+
+        return max;
     }
 }
