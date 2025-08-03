227d98c819ba6aa2be5697b595d8826e0b62e2d6
Amanrawal1305 <141016643+Amanrawal1305@users.noreply.github.com>
2025-08-04 00:34:05 +0530

    Time: 1 ms (5.57%) | Memory: 44 MB (86.77%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.java
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.java
@@ -8,24 +8,45 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+//     public ListNode deleteDuplicates(ListNode head) {
+        
+//         ListNode temp = head;
+
+//         while(temp != null)
+//         {
+//             ListNode curr = temp;
+
+//             while(curr.next != null && curr.val == curr.next.val)
+//             {
+//                 curr.next = curr.next.next;
+//             }
+
+//             if(temp.next != curr.next) temp.next = curr.next;
+//             temp = temp.next;
+//         }
+
+//         return head;
+//     }
+// }
+
+
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        
+        HashSet<Integer> set = new HashSet<>();
+
         ListNode temp = head;
+        ListNode prev = null;
 
-        while(temp != null)
-        {
-            ListNode curr = temp;
-
-            while(curr.next != null && curr.val == curr.next.val)
-            {
-                curr.next = curr.next.next;
+        while(temp!=null){
+            if(set.contains(temp.val)){
+               prev.next = temp.next;
+            }else{
+                set.add(temp.val);
+                prev = temp;
             }
-
-            if(temp.next != curr.next) temp.next = curr.next;
             temp = temp.next;
         }
-
-        return head;
+        return head;    
     }
 }
