19f9feb56c74e18b79a5f5da26c7f89131f1e9d1
Amanrawal1305 <141016643+Amanrawal1305@users.noreply.github.com>
2025-08-13 23:20:36 +0530

    Time: 4 ms (67.64%) | Memory: 44.6 MB (41.78%) - LeetSync

--- 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.java
+++ 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.java
@@ -1,16 +1,29 @@
+// class Solution {
+//     public boolean divideArray(int[] nums) {
+//         Map<Integer, Integer> counter = new HashMap<>();
+//         for (int num : nums) {
+//             counter.put(num, counter.getOrDefault(num, 0) + 1);
+//         }
+        
+//         for (int count : counter.values()) {
+//             if (count % 2 != 0) {
+//                 return false;
+//             }
+//         }
+        
+//         return true;
+//     }
+// }
+import java.util.Arrays;
+
 class Solution {
     public boolean divideArray(int[] nums) {
-        Map<Integer, Integer> counter = new HashMap<>();
-        for (int num : nums) {
-            counter.put(num, counter.getOrDefault(num, 0) + 1);
-        }
-        
-        for (int count : counter.values()) {
-            if (count % 2 != 0) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i += 2) {
+            if (nums[i] != nums[i + 1]) {
                 return false;
             }
         }
-        
         return true;
     }
 }
