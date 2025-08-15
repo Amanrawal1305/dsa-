4d0eec5369f8d561a9ccd17a4c1f068d431843ae
Amanrawal1305 <141016643+Amanrawal1305@users.noreply.github.com>
2025-08-15 23:50:24 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (35.19%) - LeetSync

--- 342-power-of-four/power-of-four.java
+++ 342-power-of-four/power-of-four.java
@@ -1,5 +1,8 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        return (n & (n - 1)) == 0 && n % 3 == 1;
+        if((n & (n - 1)) == 0 && n % 3 == 1){
+            return true;
+        }
+        return false;
     }
 }
